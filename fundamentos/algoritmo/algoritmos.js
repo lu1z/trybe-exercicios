c493bc219018b692f6abb7c8aa45cbbeca7d5236
lu1z <luiz.alberto.laus.rosa@gmail.com>
2022-09-23 20:20:09 -0300

    higgest prime

--- fundamentos/algoritmo/algoritmos.js
+++ fundamentos/algoritmo/algoritmos.js
@@ -11,14 +11,33 @@
 // for (let i = word.length - 1; i >= 0; i--) reversedWord += word.charAt(i);
 // console.log(reversedWord);
 
-let array = ["java", "javascript", "python", "html", "css"];
-let maior = array[0];
-let menor = array[0];
+// let array = ["java", "javascript", "python", "html", "css"];
+// let maior = array[0];
+// let menor = array[0];
 
-for (let str of array) {
-  if (str.length > maior.length) maior = str;
-  if (str.length < menor.length) menor = str;
+// for (let str of array) {
+//   if (str.length > maior.length) maior = str;
+//   if (str.length < menor.length) menor = str;
+// }
+
+// console.log(maior);
+// console.log(menor);
+
+let ub = 50;
+let lb = 2;
+
+if (ub === 2) console.log(2);
+else {
+  for (let curr = ub % 2 ? ub : ub - 1; curr >= lb; curr -= 2) {
+    let prime = true;
+    for (let div = 3; div < curr; div += 2)
+      if (!(curr % div)) {
+        prime = false;
+        break;
+      }
+    if(prime) {
+        console.log(curr);
+        break;
+    }
+  }
 }
-
-console.log(maior);
-console.log(menor);
