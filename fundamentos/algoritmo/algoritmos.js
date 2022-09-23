608e287981d01908042d79ef5fbe94c06f1dc009
lu1z <luiz.alberto.laus.rosa@gmail.com>
2022-09-23 19:47:36 -0300

    find biggest and smalest word in str array

--- fundamentos/algoritmo/algoritmos.js
+++ fundamentos/algoritmo/algoritmos.js
@@ -6,7 +6,21 @@
 // for (let i = 1; i <= n; i++) fat *= i;
 // console.log(fat);
 
-let word = "tryber";
-let reversedWord = "";
-for (let i = word.length - 1; i >= 0; i--) reversedWord += word.charAt(i);
-console.log(reversedWord);
+// let word = "tryber";
+// let reversedWord = "";
+// for (let i = word.length - 1; i >= 0; i--) reversedWord += word.charAt(i);
+// console.log(reversedWord);
+
+let array = ["java", "javascript", "python", "html", "css"];
+let maior = array[0];
+let menor = array[0];
+
+for(let str of array) {
+    if(str.length > maior.length)
+        maior = str;
+    if(str.length < menor.length)
+        menor = str;
+}
+
+console.log(maior);
+console.log(menor);
