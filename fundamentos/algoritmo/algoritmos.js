22e92c003450a5fa68c4d5c93596c689d4ffb9ba
lu1z <luiz.alberto.laus.rosa@gmail.com>
2022-09-23 19:36:29 -0300

    reverse word

--- fundamentos/algoritmo/algoritmos.js
+++ fundamentos/algoritmo/algoritmos.js
@@ -1,7 +1,12 @@
 // O fatorial é representado pelo sinal !
 // 4! = 4 x 3 x 2 x 1 = 24
 
-let fat = 1;
-let n = 10;
-for (let i = 1; i <= n; i++) fat *= i;
-console.log(fat);
+// let fat = 1;
+// let n = 10;
+// for (let i = 1; i <= n; i++) fat *= i;
+// console.log(fat);
+
+let word = "tryber";
+let reversedWord = "";
+for (let i = word.length - 1; i >= 0; i--) reversedWord += word.charAt(i);
+console.log(reversedWord);
