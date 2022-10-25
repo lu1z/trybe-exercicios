44320e47e655b8448cc9af633512b79a5c2b527e
lu1z <luiz.alberto.laus.rosa@gmail.com>
2022-10-24 22:54:17 -0300

    hof exer

---
+++ fundamentos/hof/gas.js
@@ -0,0 +1,68 @@
+const doragonDamaju = function () {
+  return Math.floor(Math.random() * (this.strength - 14) + 15);
+};
+const variorDamaju = function () {
+  return Math.floor(
+    Math.random() * (this.weaponDmg * this.strength - this.strength + 1) +
+      this.strength
+  );
+};
+const majouDamaju = function () {
+  return {
+    damaju:
+      this.mana < 15
+        ? "More mana is required!"
+        : Math.floor(
+            Math.random() * (this.intelligence + 1) + this.intelligence
+          ),
+    mama: this.mana < 15 ? 0 : 15,
+  };
+};
+
+const gameActions = {
+  attacku: (attaker, damageCalc, targetu) => {
+    attaker.damage = damageCalc;
+    let dmg = attaker.damage();
+    if (typeof dmg === "object" && dmg.mama > 0) attaker.mana -= dmg.mama;
+    if (typeof dmg === "object" && dmg.mama === 0) {
+      console.log(dmg.damaju);
+      return;
+    }
+    targetu.healthPoints -= typeof dmg === "number" ? dmg : dmg.damaju;
+  },
+};
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+console.log(battleMembers);
+while (
+  (mage.healthPoints > 0 || warrior.healthPoints > 0) &&
+  dragon.healthPoints > 0
+) {
+  gameActions.attacku(mage, majouDamaju, dragon);
+  console.log(battleMembers);
+  gameActions.attacku(warrior, variorDamaju, dragon);
+  console.log(battleMembers);
+  gameActions.attacku(dragon, doragonDamaju, warrior);
+  console.log(battleMembers);
+}
+console.log(battleMembers);
